8c06ca82cf2cc7db27ae0e4810f0c1fc72b6ba78
Mohamed Abdeljalil ROUANE <rouane.mohamedabdeljalil@ine.inpt.ma>
2021-05-06 13:34:07 +0000

    create patient model

---
+++ models/patient.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import Appoitment from "./appoitment";
+
+
+const patientSchema = mongoose.Schema({
+    avatar: String,
+    name: {
+        firstName: String,
+        lastName: String
+    },
+    email: String,
+    age: Number,
+    password: String,
+    phoneNumber: String,
+    sendRequest: [Appoitment],
+    appoitments: [Appoitment],
+    history: []
+});
+
+const Patient = mongoose.model("Patient", patientSchema);
+
+module.exports = Patient;
+
