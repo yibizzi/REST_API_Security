b07dc8182d57396d80716dc085b64af098b68681
Mohamed Abdeljalil ROUANE <rouane.mohamedabdeljalil@ine.inpt.ma>
2021-05-11 15:30:02 +0000

    create admin routes

---
+++ app/routes/admin.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const adminController = require("../controllers/admin");
+const {auth} = require("../middlewares/auth");
+
+//Auth:
+router.post('/auth/signup',adminController.signup);
+router.post('/auth/login', adminController.login);
+router.post('/auth/forget-password',adminController.forgetPassword);
+router.put('/auth/reset-password',adminController.resetPassword);
+router.get('/auth/logout', adminController.logout);
+
+
+//Other routes:
+router.get("/", adminController.getAdmins);
+router.get("/:adminId", adminController.getAdminById);
+router.put("/:adminId", adminController.updateAdmin);
+router.delete("/:adminId",  adminController.deleteAdmin);
+
+
+module.exports = router;
