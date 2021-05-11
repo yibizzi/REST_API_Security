52a9d79496ecf756e94c4c5db46cded20e0fcc67
Mohamed Abdeljalil ROUANE <rouane.mohamedabdeljalil@ine.inpt.ma>
2021-05-11 15:26:44 +0000

    update appointment routes

--- app/routes/appointment.js
+++ app/routes/appointment.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const appointmentController = require("../controllers/appointment");
+const auth = require("../middlewares/auth");
 
 router.get("/:appointmentId", appointmentController.getAppointmentbyId);
-router.post("/", appointmentController.askforAppointment);
+router.post("/",auth.patient_Auth_With_Id_Body, appointmentController.askforAppointment);
 router.get("/", appointmentController.getAppointments);
 router.put("/:appointmentId", appointmentController.updateAppointment);
 router.delete("/:appointmentId", appointmentController.deleteAppointment);
