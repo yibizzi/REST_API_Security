f7c353bb378d04819e04126e5bf550c7ac8bf992
Moadbenammi <moadbenammi@gmail.com>
2021-05-22 22:07:16 +0100

    Enhace code

--- app/routes/patient.js
+++ app/routes/patient.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const patientController = require("../controllers/patient");
-const paymentController = require("../controllers/payment");
-const appointmentController = require("../controllers/appointment");
 const { auth } = require('../middlewares/auth');
 
 //Auth
@@ -13,16 +11,14 @@ router.put('/auth/reset-password',patientController.resetPassword);
 
 
 //Other Routes
-router.get("/", patientController.getPatients);
-router.get("/:patientId", patientController.getPatientById);
-router.post("/:patientId/rate-doctor", patientController.rateDoctor);
-router.put("/:patientId/rate-doctor", patientController.updateRating);
-// router.post("/:patientId/payment", auth, paymentController.pay);
-router.get("/:patientId/appointments", patientController.patientAppointments);
-router.get("/:patientId/send-requests", patientController.patientSendRequests);
-router.put("/:patientId", patientController.updatePatientInfo);
-router.delete("/:patientId", patientController.deletePatient);
-
+router.get("/", auth, patientController.getPatients);
+router.get("/:patientId", auth, patientController.getPatientById);
+router.post("/:patientId/rate-doctor", auth, patientController.rateDoctor);
+router.put("/:patientId/rate-doctor", auth, patientController.updateRating);
+router.get("/:patientId/appointments", auth, patientController.patientAppointments);
+router.get("/:patientId/send-requests", auth, patientController.patientSendRequests);
+router.put("/:patientId", auth, patientController.updatePatientInfo);
+router.delete("/:patientId", auth, patientController.deletePatient);
 
 
 module.exports = router;
--- app/routes/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const paymentController = require("../controllers/payment");
-const { auth } = require('../middlewares/auth');
-
-router.post("/", paymentController.pay);
-
-
-module.exports = router;
